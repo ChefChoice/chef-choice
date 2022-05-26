0108e9b57c9bd3b8a01a017d5e63420c8a6ddd5c
Seung Woo (Paul) Ji <wincow.ji@gmail.com>
2022-05-26 00:12:24 -0400

    #46 Added Navigation Bar

---
+++ components/layout/Layout.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+
+import Navbar from './Navbar';
+
+const Layout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <>
+      <Navbar />
+      <main>{children}</main>
+    </>
+  );
+};
+
+export default Layout;
---
+++ components/layout/Navbar.tsx
@@ -0,0 +1,41 @@
+import Link from 'next/link';
+
+import Logo from './Logo';
+
+const Navbar = () => {
+  return (
+    <div className="flex items-center max-w-screen-2xl mx-auto px-5 py-3 bg-white">
+      <div className="mr-8">
+        <Link href="/">
+          <a>
+            <Logo />
+          </a>
+        </Link>
+      </div>
+
+      <div className="flex items-center justify-between w-full">
+        <nav>
+          <ul>
+            <li>
+              <Link href="/">
+                <a className="text-black font-semibold text-xl">Become a Chef</a>
+              </Link>
+            </li>
+          </ul>
+        </nav>
+        <div className="flex items-center gap-x-8">
+          <Link href="/">
+            <a className="text-black font-semibold text-xl">Help</a>
+          </Link>
+          <Link href="/">
+            <a className="text-white font-semibold text-xl px-6 py-1 border-none rounded-lg bg-green-light">
+              Log In
+            </a>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
