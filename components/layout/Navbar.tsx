fa44a1ee8aecac5274f7f11611f58d7ff0493691
dttg-nguyen <dttg-nguyen@users.noreply.github.com>
2022-06-01 16:58:34 -0400

    Revert "Add user authorization to render Navbar accordingly. Only homechef's navbar for now."
    This reverts commit a769f783aa2231213d1013eb0c7637a71aa73a09.

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -1,23 +1,12 @@
-import { User } from '@supabase/supabase-js';
 import Link from 'next/link';
-import { useEffect, useState } from 'react';
-import { supabase } from '../../utils/supabaseClient';
+
 import Logo from './Logo';
 
-// Added user authorization
-// TODO: Add user type check
-
 const Navbar = () => {
-  const [user, setUser] = useState<User | null>(null);
-
-  useEffect(() => {
-    setUser(supabase.auth.user());
-  }, []);
-
   return (
     <div className="flex items-center max-w-screen-2xl mx-auto px-5 py-3 bg-white">
       <div className="mr-8">
-        <Link href="/main-menu">
+        <Link href="/">
           <a>
             <Logo />
           </a>
@@ -27,51 +16,22 @@ const Navbar = () => {
       <div className="flex items-center justify-between w-full">
         <nav>
           <ul>
-            {user ? (
-              <li>
-                <Link href="/main-menu">
-                  <a className="text-black font-semibold text-xl px-5">Home</a>
-                </Link>
-
-                <Link href="/">
-                  <a className="text-black font-semibold text-xl px-5">Orders</a>
-                </Link>
-
-                <Link href="/marketplace">
-                  <a className="text-black font-semibold text-xl">Marketplace</a>
-                </Link>
-              </li>
-            ) : (
-              <li>
-                <Link href="/">
-                  <a className="text-black font-semibold text-xl">Become a Chef</a>
-                </Link>
-              </li>
-            )}
+            <li>
+              <Link href="/">
+                <a className="text-black font-semibold text-xl">Become a Chef</a>
+              </Link>
+            </li>
           </ul>
         </nav>
         <div className="flex items-center gap-x-8">
-          <Link href="#">
+          <Link href="/">
             <a className="text-black font-semibold text-xl">Help</a>
           </Link>
-          {user ? (
-            <>
-              <Link href="#">
-                <a className="text-black font-semibold text-xl">Logout</a>
-              </Link>
-              <Link href="/profile">
-                <a className="text-white font-semibold text-xl px-6 py-1 border-none rounded-lg bg-green-light">
-                  Profile
-                </a>
-              </Link>
-            </>
-          ) : (
-            <Link href="/signin">
-              <a className="text-white font-semibold text-xl px-6 py-1 border-none rounded-lg bg-green-light">
-                Log In
-              </a>
-            </Link>
-          )}
+          <Link href="/signin">
+            <a className="text-white font-semibold text-xl px-6 py-1 border-none rounded-lg bg-green-light">
+              Log In
+            </a>
+          </Link>
         </div>
       </div>
     </div>
