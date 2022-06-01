6accd6b77ca18ee3996e4b2b6bc065f6fec139cc
dttg-nguyen <dttg-nguyen@users.noreply.github.com>
2022-05-31 21:38:03 -0400

    Fix data types, Polish UI and Add routes to profile

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,24 +1,33 @@
 import Head from 'next/head';
 import Link from 'next/link';
-import { EyeIcon, PencilIcon, XCircleIcon } from '@heroicons/react/outline';
 import { useState, useEffect } from 'react';
 import { supabase } from '../utils/supabaseClient';
+import { EyeIcon, PencilIcon, XCircleIcon } from '@heroicons/react/outline';
 
 export default function Profile() {
   const [loading, setLoading] = useState(true);
-  const [certs, setCert] = useState(null);
+  const [certs, setCert] = useState<Certificate[]>([]);
+
+  type Certificate = {
+    id: number;
+    homechef_id: number;
+    cert_type: string;
+    cert_name: string;
+    cert_expirydate: Date;
+    cert_image: string;
+  };
 
   useEffect(() => {
     getCert();
   });
 
   async function getCert() {
-    if (certs == null) {
+    if (certs.length == 0) {
       try {
         setLoading(true);
 
         let { data, error, status } = await supabase
-          .from('Certificate')
+          .from<Certificate>('Certificate')
           .select(`*`)
           // select by homechef_id
           .eq('homechef_id', 1);
@@ -30,8 +39,8 @@ export default function Profile() {
         if (data) {
           setCert(data);
         }
-      } catch (error) {
-        alert(error.message);
+      } catch (err) {
+        console.log(err);
       } finally {
         setLoading(false);
       }
@@ -63,37 +72,58 @@ export default function Profile() {
         </div>
 
         <div className="w-full">
+          {loading ? 'Loading...' : ''}
           {certs &&
-            certs.map((cert) => {
+            certs.map((cert: Certificate) => {
               return (
-                <div className="grid grid-cols-8 gap-2 py-2 px-3 text-lg" key={cert.id}>
+                <div className="grid grid-cols-6 gap-2 py-2 px-3 text-lg" key={cert.id}>
                   <div className="col-span-2">
                     <p className="break-words">{cert.cert_name}</p>
                   </div>
-                  <div className="col-span-3 mx-auto">
-                    <p className="break-words">Expiry Date: {cert.cert_expirydate}</p>
+                  <div className="col-span-2 break-words">
+                    <i>Valid Until:</i> {cert.cert_expirydate.toString()}
                   </div>
-                  <Link
-                    href={{ pathname: '/view-certificate', query: { cert_image: cert.cert_image } }}
-                  >
-                    <a className="ml-auto">
-                      <EyeIcon className="h-5 w-5" />
-                    </a>
-                  </Link>
-                  <Link href="#">
-                    <a className="ml-auto">
-                      <PencilIcon className="h-5 w-5" />
-                    </a>
-                  </Link>
 
-                  {/* Check if it is allowed to delete the certificate */}
-                  {cert.cert_type != 'Required' && (
-                    <Link href="#">
-                      <a className="ml-auto">
-                        <XCircleIcon className="h-5 w-5" />
+                  <div className="break-words">
+                    {cert.cert_type == 'Required' ? (
+                      <p>{cert.cert_type}</p>
+                    ) : (
+                      <p>
+                        <i>{cert.cert_type}</i>
+                      </p>
+                    )}
+                  </div>
+
+                  <div className="grid grid-rows-1 grid-flow-col ml-auto">
+                    <Link
+                      href={{
+                        pathname: '/view-certificate',
+                        query: { cert_image: cert.cert_image },
+                      }}
+                    >
+                      <a className="mr-8" target="_blank">
+                        <EyeIcon className="h-6 w-6" />
                       </a>
                     </Link>
-                  )}
+                    <Link href="/edit-certificate">
+                      <a className="mr-8">
+                        <PencilIcon className="h-6 w-6" />
+                      </a>
+                    </Link>
+
+                    {/* Check if it is allowed to delete the certificate */}
+                    <Link href="">
+                      {cert.cert_type == 'Required' ? (
+                        <a className="ml-6">
+                          <XCircleIcon className="h-6 w-6 hidden" />
+                        </a>
+                      ) : (
+                        <a className="ml-auto">
+                          <XCircleIcon className="h-6 w-6" />
+                        </a>
+                      )}
+                    </Link>
+                  </div>
                 </div>
               );
             })}
