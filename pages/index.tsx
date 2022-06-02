81d10c17607420d02a09394b0316bad835d62b9c
JH <>
2022-06-02 00:25:46 -0400

    added titles

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import { supabase } from '../utils/supabaseClient';
 import router, { useRouter } from 'next/router';
 import { useState, useEffect } from 'react';
 import Link from 'next/link';
+import Head from 'next/head';
 
 const Home: NextPage = () => {
   // placeholder for demonstrating user account management
@@ -16,34 +17,40 @@ const Home: NextPage = () => {
 
   return (
     <>
-      <div className="bg-gray-200 max-w-screen-2xl flex h-screen mx-auto">
-        <div className="max-w-sm m-auto bg-white px-4 py-4">
-          <div>{welcomeMessage}</div>
-          <div>
-            <Link href="">
-              <a
-                onClick={async (e) => {
-                  e.preventDefault();
-                  const { error } = await supabase.auth.signOut();
-                  if (error) {
-                    document.getElementById('loginErrorField')!.innerHTML = error.message;
-                    throw error;
-                  }
-                  router.push('/signin');
-                }}
-                className="text-white font-semibold text-xl px-6 py-1 mr-2 border-none rounded-lg border-green-light hover:border-green-hover bg-green-light hover:bg-green-hover"
-              >
-                Logout
-              </a>
-            </Link>
-            <Link href="/profile">
-              <a className="text-white font-semibold text-xl px-6 py-1 border-none rounded-lg border-green-light hover:border-green-hover bg-green-light hover:bg-green-hover">
-                Profile
-              </a>
-            </Link>
+      <Head>
+        <title>Index</title>
+        <meta content="width=device-width, initial-scale=1" name="viewport" />
+      </Head>
+      <main>
+        <div className="bg-gray-200 max-w-screen-2xl flex h-screen mx-auto">
+          <div className="max-w-sm m-auto bg-white px-4 py-4">
+            <div>{welcomeMessage}</div>
+            <div>
+              <Link href="">
+                <a
+                  onClick={async (e) => {
+                    e.preventDefault();
+                    const { error } = await supabase.auth.signOut();
+                    if (error) {
+                      document.getElementById('loginErrorField')!.innerHTML = error.message;
+                      throw error;
+                    }
+                    router.push('/signin');
+                  }}
+                  className="text-white font-semibold text-xl px-6 py-1 mr-2 border-none rounded-lg border-green-light hover:border-green-hover bg-green-light hover:bg-green-hover"
+                >
+                  Logout
+                </a>
+              </Link>
+              <Link href="/profile">
+                <a className="text-white font-semibold text-xl px-6 py-1 border-none rounded-lg border-green-light hover:border-green-hover bg-green-light hover:bg-green-hover">
+                  Profile
+                </a>
+              </Link>
+            </div>
           </div>
         </div>
-      </div>
+      </main>
     </>
   );
 };
--- pages/signup.tsx
+++ pages/signup.tsx
@@ -2,6 +2,7 @@
 
 import { supabase } from '../utils/supabaseClient';
 import { ChangeEvent, FormEvent, useState } from 'react';
+import Head from 'next/head';
 
 export default function SignUp() {
   const [email, setEmail] = useState('');
@@ -19,38 +20,44 @@ export default function SignUp() {
   // TODO: Add password re-entry for verification
   return (
     <>
-      <div className="bg-gray-200 max-w-screen-2xl flex h-screen mx-auto">
-        <div className="max-w-sm m-auto bg-white px-4 py-4">
-          <div className="px-1 mb-4">
-            <p className="text-2xl font-sans font-bold">Register</p>
+      <Head>
+        <title>Sign Up</title>
+        <meta content="width=device-width, initial-scale=1" name="viewport" />
+      </Head>
+      <main>
+        <div className="bg-gray-200 max-w-screen-2xl flex h-screen mx-auto">
+          <div className="max-w-sm m-auto bg-white px-4 py-4">
+            <div className="px-1 mb-4">
+              <p className="text-2xl font-sans font-bold">Register</p>
+            </div>
+            <form onSubmit={handleSubmit} className="mb-2">
+              <input
+                className="w-full rounded px-3 py-2 mb-4 bg-gray-200 border-gray-200 border-2 hover:border-green-light focus:outline-none focus:bg-white focus:border-green-light"
+                placeholder="Email Address"
+                id="input-email"
+                type="email"
+                value={email}
+                onChange={(e: ChangeEvent<HTMLInputElement>) => setEmail(e.target.value)}
+                required
+              />
+              <input
+                className="w-full rounded px-3 py-2 mb-2 bg-gray-200 border-gray-200 border-2 hover:border-green-light focus:outline-none focus:bg-white focus:border-green-light"
+                placeholder="Password"
+                id="input-password"
+                type="password"
+                value={password}
+                onChange={(e: ChangeEvent<HTMLInputElement>) => setPassword(e.target.value)}
+              />
+              <button
+                className="block max-w-sm bg-green-light hover:bg-green-hover border-green-light hover:border-green-hover py-2 px-2 text-white rounded w-full"
+                type="submit"
+              >
+                Login
+              </button>
+            </form>
           </div>
-          <form onSubmit={handleSubmit} className="mb-2">
-            <input
-              className="w-full rounded px-3 py-2 mb-4 bg-gray-200 border-gray-200 border-2 hover:border-green-light focus:outline-none focus:bg-white focus:border-green-light"
-              placeholder="Email Address"
-              id="input-email"
-              type="email"
-              value={email}
-              onChange={(e: ChangeEvent<HTMLInputElement>) => setEmail(e.target.value)}
-              required
-            />
-            <input
-              className="w-full rounded px-3 py-2 mb-2 bg-gray-200 border-gray-200 border-2 hover:border-green-light focus:outline-none focus:bg-white focus:border-green-light"
-              placeholder="Password"
-              id="input-password"
-              type="password"
-              value={password}
-              onChange={(e: ChangeEvent<HTMLInputElement>) => setPassword(e.target.value)}
-            />
-            <button
-              className="block max-w-sm bg-green-light hover:bg-green-hover border-green-light hover:border-green-hover py-2 px-2 text-white rounded w-full"
-              type="submit"
-            >
-              Login
-            </button>
-          </form>
         </div>
-      </div>
+      </main>
     </>
   );
 }
