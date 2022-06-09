4883e36b910bccdd0372b798082e8760498b14a8
dttg-nguyen <dttg-nguyen@users.noreply.github.com>
2022-06-08 21:38:06 -0400

    Refactor to use Supabase's withPageAuth for user authorization

--- pages/main-menu.tsx
+++ pages/main-menu.tsx
@@ -1,31 +1,27 @@
-// import { User } from '@supabase/supabase-js';
+import { User, withPageAuth } from '@supabase/supabase-auth-helpers/nextjs';
 import Head from 'next/head';
 import Link from 'next/link';
 import { useEffect, useState } from 'react';
 import { supabase } from '../utils/supabaseClient';
 
-import { useRouter } from 'next/router';
-
-import { useUser } from '../lib/UserContext';
-import SignIn from './signin';
-
 // TODO: Check user types; Add navigation for Orders and Search Dishes
-// TODO: integrate global userContext later
+
+export const getServerSideProps = withPageAuth({
+  redirectTo: '/signin',
+});
 
 export default function MainMenu() {
   const [loading, setLoading] = useState(false);
   const [username, setUsername] = useState(null);
-  // const { push } = useRouter();
-  // const [session, setSession] = useState<any | null>(null);
-  const { user } = useUser();
+  const [user, setUser] = useState<User | null>(null);
 
   useEffect(() => {
-    // setSession(supabase.auth.session());
-    if (user) getUsername();
+    getUsername();
   });
 
   async function getUsername() {
     try {
+      setUser(supabase.auth.user());
       if (user) {
         let { data, error, status } = await supabase
           .from('HomeChef')
@@ -50,57 +46,47 @@ export default function MainMenu() {
 
   return (
     <>
-      {!user ? (
-        <Link href="/signin">
-          <a>Sign in</a>
-        </Link>
-      ) : !loading ? (
-        <p>Loading...</p>
-      ) : (
-        <>
-          <Head>
-            <title>Main Menu</title>
-            <meta content="width=device-width, initial-scale=1" name="viewport" />
-          </Head>
+      <Head>
+        <title>Main Menu</title>
+        <meta content="width=device-width, initial-scale=1" name="viewport" />
+      </Head>
 
-          <div className="mx-auto">
-            <main className="background-image h-screen w-screen py-4 flex flex-col justify-center items-center">
-              <h1 className="font-headline font-medium text-4xl break-words">
-                Welcome to ChefChoice, {username ? username : ''}
-                <hr style={{ borderTop: '3px solid #000000 ' }} />
-              </h1>
-              <div className="grid grid-rows-3 grid-flow-col gap-2 pt-10 justify-items-center items-center w-96">
-                {/* TODO: Change the option cards according to the type of user*/}
-                {2 > 1 ? (
-                  <Link href="#">
-                    <a className="flex justify-center items-center mx-auto py-2 px-2 bg-green-light text-white text-xl rounded-lg hover:bg-green-hover md:w-full w-3/4 h-20">
-                      <h2>Orders</h2>
-                    </a>
-                  </Link>
-                ) : (
-                  <Link href="#">
-                    <a className="flex justify-center items-center mx-auto py-2 px-2 bg-green-light text-white text-xl rounded-lg hover:bg-green-hover md:w-full w-3/4 h-20">
-                      <h2>Search Dishes</h2>
-                    </a>
-                  </Link>
-                )}
+      <div className="mx-auto">
+        <main className="background-image h-screen w-screen py-4 flex flex-col justify-center items-center">
+          <h1 className="font-headline font-medium text-4xl break-words">
+            Welcome to ChefChoice, {username ? username : ''}
+            <hr style={{ borderTop: '3px solid #000000 ' }} />
+          </h1>
+          <div className="grid grid-rows-3 grid-flow-col gap-2 pt-10 justify-items-center items-center w-96">
+            {/* TODO: Change the option cards according to the type of user*/}
+            {2 > 1 ? (
+              <Link href="#">
+                <a className="flex justify-center items-center mx-auto py-2 px-2 bg-green-light text-white text-xl rounded-lg hover:bg-green-hover md:w-full w-3/4 h-20">
+                  <h2>Orders</h2>
+                </a>
+              </Link>
+            ) : (
+              <Link href="#">
+                <a className="flex justify-center items-center mx-auto py-2 px-2 bg-green-light text-white text-xl rounded-lg hover:bg-green-hover md:w-full w-3/4 h-20">
+                  <h2>Search Dishes</h2>
+                </a>
+              </Link>
+            )}
 
-                <Link href="/marketplace">
-                  <a className="flex justify-center items-center mx-auto py-2 px-2 bg-green-light text-white text-xl rounded-lg hover:bg-green-hover md:w-full w-3/4 h-20">
-                    <h2>Marketplace Management</h2>
-                  </a>
-                </Link>
+            <Link href="/marketplace">
+              <a className="flex justify-center items-center mx-auto py-2 px-2 bg-green-light text-white text-xl rounded-lg hover:bg-green-hover md:w-full w-3/4 h-20">
+                <h2>Marketplace Management</h2>
+              </a>
+            </Link>
 
-                <Link href="/profile">
-                  <a className="flex justify-center items-center mx-auto py-2 px-2 bg-green-light text-white text-xl rounded-lg hover:bg-green-hover md:w-full w-3/4 h-20">
-                    <h2>My Account</h2>
-                  </a>
-                </Link>
-              </div>
-            </main>
+            <Link href="/profile">
+              <a className="flex justify-center items-center mx-auto py-2 px-2 bg-green-light text-white text-xl rounded-lg hover:bg-green-hover md:w-full w-3/4 h-20">
+                <h2>My Account</h2>
+              </a>
+            </Link>
           </div>
-        </>
-      )}
+        </main>
+      </div>
     </>
   );
 }
--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,23 +1,21 @@
 import { EyeIcon, PencilIcon, XCircleIcon } from '@heroicons/react/outline';
+import { withPageAuth } from '@supabase/supabase-auth-helpers/nextjs';
 import { User } from '@supabase/supabase-js';
 import Head from 'next/head';
 import Link from 'next/link';
-import { useRouter } from 'next/router';
-
 import { useEffect, useState } from 'react';
-import Header from '../components/common/Heading';
+import Heading from '../components/common/Heading';
 import { Certificate } from '../models/Certificate';
 import { supabase } from '../utils/supabaseClient';
-import SignIn from './signin';
 
-// TODO: integrate global userContext later
+export const getServerSideProps = withPageAuth({
+  redirectTo: '/signin',
+});
 
 export default function Profile() {
-  const [loading, setLoading] = useState(false);
+  // const [loading, setLoading] = useState(false);
   const [user, setUser] = useState<User | null>(null);
   const [certificates, setCertificates] = useState<Certificate[]>([]);
-
-  const { push } = useRouter();
 
   useEffect(() => {
     getCertificates();
@@ -26,7 +24,7 @@ export default function Profile() {
   async function getCertificates() {
     if (certificates.length == 0) {
       try {
-        setLoading(true);
+        // setLoading(true);
         setUser(supabase.auth.user());
 
         if (user) {
@@ -48,99 +46,94 @@ export default function Profile() {
         }
       } catch (err) {
         console.log(err);
-      } finally {
-        setLoading(false);
       }
+      // } finally {
+      //   setLoading(false);
+      // }
     }
   }
 
   return (
     <>
-      {!user ? (
-        <SignIn />
-      ) : (
-        <>
-          <Head>
-            <title>Profile</title>
-            <meta content="width=device-width, initial-scale=1" name="viewport" />
-          </Head>
+      <Head>
+        <title>Profile</title>
+        <meta content="width=device-width, initial-scale=1" name="viewport" />
+      </Head>
 
-          <main className="flex flex-col h-screen w-full py-20 px-10">
-            <Header
-              title={'Certification Management'}
-              optionalNode={
-                <Link
-                  href={{
-                    pathname: '/certificate-management/add-certificate',
-                    query: {
-                      id: user.id,
-                    },
-                  }}
-                >
-                  <button className="bg-white border-solid border-black border-2 hover:ring font-medium px-8 rounded">
-                    Add
-                  </button>
-                </Link>
-              }
-              optionalNodeRightAligned={true}
-            />
+      <main className="flex flex-col h-screen w-full py-20 px-10">
+        <Heading
+          title={'Certification Management'}
+          optionalNode={
+            <Link
+              href={{
+                pathname: '/certificate-management/add-certificate',
+                query: {
+                  id: user ? user.id : null,
+                },
+              }}
+            >
+              <button className="bg-white py-1 text-lg border-solid border-black border-2 hover:ring font-medium px-8 rounded">
+                Add
+              </button>
+            </Link>
+          }
+          optionalNodeRightAligned={true}
+        />
 
-            <div className="w-full">
-              {/* {loading ? 'Loading...' : ''} */}
+        <div className="w-full">
+          {/* {loading ? 'Loading...' : ''} */}
 
-              {certificates.length === 0
-                ? 'No records found'
-                : certificates.map((cert: Certificate) => {
-                    return (
-                      <div className="grid grid-cols-6 gap-2 py-2 px-3 text-lg" key={cert.id}>
-                        <div className="col-span-2">
-                          <p className="break-words">{cert.name}</p>
-                        </div>
-                        <div className="col-span-2 break-words">
-                          <i>Valid Until:</i> {cert.expirydate.toString()}
-                        </div>
+          {certificates.length === 0
+            ? 'No records found'
+            : certificates.map((cert: Certificate) => {
+                return (
+                  <div className="grid grid-cols-6 gap-2 py-2 px-3 text-lg" key={cert.id}>
+                    <div className="col-span-2">
+                      <p className="break-words">{cert.name}</p>
+                    </div>
+                    <div className="col-span-2 break-words">
+                      <i>Valid Until:</i> {cert.expirydate.toString()}
+                    </div>
 
-                        <div className="break-words">
-                          <p>{cert.type}</p>
-                        </div>
+                    <div className="break-words">
+                      <p>{cert.type}</p>
+                    </div>
 
-                        <div className="grid grid-rows-1 grid-flow-col ml-auto">
-                          <Link
-                            href={{
-                              pathname: '/certificate-management/view-certificate',
-                              query: { cert_image: cert.image },
-                            }}
-                          >
-                            <a className="mr-8" target="_blank">
-                              <EyeIcon className="h-6 w-6" />
-                            </a>
-                          </Link>
-                          <Link href="/edit-certificate">
-                            <a className="mr-8">
-                              <PencilIcon className="h-6 w-6" />
-                            </a>
-                          </Link>
+                    <div className="grid grid-rows-1 grid-flow-col ml-auto">
+                      <Link
+                        href={{
+                          pathname: '/certificate-management/view-certificate',
+                          query: { cert_image: cert.image },
+                        }}
+                      >
+                        <a className="mr-8" target="_blank">
+                          <EyeIcon className="h-6 w-6" />
+                        </a>
+                      </Link>
+                      <Link href="/edit-certificate">
+                        <a className="mr-8">
+                          <PencilIcon className="h-6 w-6" />
+                        </a>
+                      </Link>
 
-                          {/* Check if it is allowed to delete the certificate */}
-                          <Link href="">
-                            {cert.type == 'Required' ? (
-                              <a className="ml-6">
-                                <XCircleIcon className="h-6 w-6 hidden" />
-                              </a>
-                            ) : (
-                              <a className="ml-auto">
-                                <XCircleIcon className="h-6 w-6" />
-                              </a>
-                            )}
-                          </Link>
-                        </div>
-                      </div>
-                    );
-                  })}
-            </div>
-          </main>
-        </>
-      )}
+                      {/* Check if it is allowed to delete the certificate */}
+                      <Link href="">
+                        {cert.type == 'Required' ? (
+                          <a className="ml-6">
+                            <XCircleIcon className="h-6 w-6 hidden" />
+                          </a>
+                        ) : (
+                          <a className="ml-auto">
+                            <XCircleIcon className="h-6 w-6" />
+                          </a>
+                        )}
+                      </Link>
+                    </div>
+                  </div>
+                );
+              })}
+        </div>
+      </main>
     </>
   );
 }
