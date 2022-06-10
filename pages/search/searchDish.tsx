05b5969d112f6d6634bc12ddadace6fea38e0a0a
Seung Woo (Paul) Ji <wincow.ji@gmail.com>
2022-06-10 14:04:18 -0400

    Added pagination for the search results

--- pages/search/searchDish.tsx
+++ pages/search/searchDish.tsx
@@ -11,74 +11,80 @@ import Loading from '../../components/search/Loading';
 const SearchDish: NextPage = () => {
   const DISH_IMAGE_PUBLIC_URL =
     'https://nwcvvpfhfsturkjenths.supabase.co/storage/v1/object/public/dish-images';
+  const DISH_DISPLAY_LIMIT = 2; // the number of displayed dishes will be n + 1
 
   const termRef = useRef<HTMLInputElement>(null);
 
   const [isLoading, setLoading] = useState(false);
+  const [hasMore, setHasMore] = useState(false);
   const [searchTerm, setSearchTerm] = useState('');
   const [result, setResult] = useState<Array<any>>([]);
+  const [page, setPage] = useState(0);
 
   useEffect(() => {
     async function getResult() {
-      const { data: dishData, error: dishError } = await supabase
-        .rpc('search_dishes', { dish_term: searchTerm })
-        .order('dish_name');
+      const { from, to } = getPagination(page, DISH_DISPLAY_LIMIT);
+
+      setLoading(true);
+
+      const { data: dishData, error: dishError } = searchTerm
+        ? await supabase
+            .rpc('search_dishes', { dish_term: searchTerm })
+            .order('dish_name')
+            .range(from, to)
+        : await supabase.from('dishinfo').select().order('dish_name').range(from, to);
 
       if (dishError) throw dishError.message;
-      if (dishData) {
-        setResult(dishData);
-        setLoading(false);
+
+      if (dishData.length) {
+        setResult([...result, ...dishData]);
+        setHasMore(true);
+      } else {
+        setHasMore(false);
       }
+
+      setLoading(false);
     }
 
-    async function getDishes() {
-      const { data: dishData, error: dishError } = await supabase
-        .from('dishinfo')
-        .select()
-        .order('dish_name');
+    getResult();
+  }, [searchTerm, page]);
 
-      if (dishError) throw dishError.message;
-      if (dishData) {
-        setResult(dishData);
-        setLoading(false);
-      }
-    }
+  const getPagination = (page: number, size: number) => {
+    const from = page ? page * (size + 1) : 0;
+    const to = page ? from + size : size;
 
-    setLoading(true);
-    searchTerm ? getResult() : getDishes();
-  }, [searchTerm]);
+    return { from, to };
+  };
 
   const handleKeyPress = (e: KeyboardEvent) => {
     if (e.key === 'Enter') {
       setSearchTerm(termRef.current!.value);
+      setPage(0);
+      setResult([]);
     }
   };
 
   const renderResults = () => {
-    if (isLoading) {
-      return <Loading />;
+    if (!isLoading && !result.length) {
+      return (
+        <div className="flex flex-col justify-center items-center w-full">
+          <h2 className="text-2xl font-semibold">No items found</h2>
+          <p className="text-lg mt-3">Please try with another search term</p>
+        </div>
+      );
     }
 
-    if (result.length) {
-      return result.map((dish: any) => (
-        <SearchRow
-          key={dish.dish_id}
-          dishId={dish.dish_id}
-          dishName={dish.dish_name}
-          dishPrice={dish.dish_price}
-          chefName={dish.name}
-          href="#"
-          imageSrc={`${DISH_IMAGE_PUBLIC_URL}/${dish.dish_image}`}
-        />
-      ));
-    }
-
-    return (
-      <div className="flex flex-col justify-center items-center w-full">
-        <h2 className="text-2xl font-semibold">No items found</h2>
-        <p className="text-lg mt-3">Please try with another search term</p>
-      </div>
-    );
+    return result.map((dish: any) => (
+      <SearchRow
+        key={dish.dish_id}
+        dishId={dish.dish_id}
+        dishName={dish.dish_name}
+        dishPrice={dish.dish_price}
+        chefName={dish.name}
+        href="#"
+        imageSrc={`${DISH_IMAGE_PUBLIC_URL}/${dish.dish_image}`}
+      />
+    ));
   };
 
   return (
@@ -95,6 +101,18 @@ const SearchDish: NextPage = () => {
           handleKeyPress={handleKeyPress}
         />
         <div className="grid w-2/3 gap-10 grid-cols-1 mt-11">{renderResults()}</div>
+        {isLoading ? (
+          <Loading />
+        ) : (
+          hasMore && (
+            <button
+              className="text-white font-semibold text-xl px-6 py-1 mt-10 rounded-lg border-green-light hover:border-green-hover bg-green-light hover:bg-green-hover"
+              onClick={() => setPage(page + 1)}
+            >
+              Show more
+            </button>
+          )
+        )}
       </div>
     </>
   );
