787e82c55be8c0d0a512089fa50759a9861e3161
Alastair Odhiambo <alastairodhiambo@outlook.com>
2022-07-18 18:12:14 -0400

    fix: linting error

--- pages/order-management/order/[id].tsx
+++ pages/order-management/order/[id].tsx
@@ -79,9 +79,9 @@ const Order: NextPage = ({ page }: any) => {
                         key={i}
                         quantity={orderDish.quantity}
                         title={orderDish.Dish.dish_name}
-                        price={
+                        price={`${
                           Math.round(orderDish.Dish.dish_price * orderDish.quantity * 100) / 100
-                        }
+                        }`}
                         orderDish={orderDish}
                       ></CartItem>
                     ))}
