c6b98ee45504a18b8d95f27a07d237751abd93a4
Alastair Odhiambo <alastairodhiambo@outlook.com>
2022-08-10 15:03:20 -0400

    update: UI tweaks for Mobile

--- components/orders/OrderList.tsx
+++ components/orders/OrderList.tsx
@@ -48,74 +48,77 @@ const OrderList = ({ type, orders, isHomeChef, refresh, setRefresh }: IOrderList
         orders.map((order: Order, i: number) => {
           const time: Date = new Date(Date.parse(order.time));
           return (
-            <div key={i} className="flex gap-3 py-2">
-              <div className="flex grow gap-5">
+            <div key={i} className="py-2 md:flex">
+              <div className="mb-4 flex grow gap-5 md:mb-0">
                 <div className="w-20 text-xl">{`#${order.id}`}</div>
                 <div className="w-30 text-xl">{time.toDateString()}</div>
                 <div className="w-20 text-xl">{`$${order.total?.toFixed(2)}`}</div>
               </div>
-              <div
-                onClick={() => handleViewDetails(order.id)}
-                className="w-full max-w-fit cursor-pointer overflow-hidden rounded border-2 border-solid border-black shadow-lg hover:bg-green-hover hover:ring"
-              >
-                <div className="py-2 px-2 text-center">
-                  <a className="xs:text-x font-bold lg:text-base">View Details</a>
-                </div>
-              </div>
-              {type === ORDER_TYPE.PAST_ORDERS && (
+              <div className="ml-auto flex grow justify-end gap-x-3 py-2">
                 <div
-                  onClick={() => handleReview(order.homechef_id)}
+                  onClick={() => handleViewDetails(order.id)}
                   className="w-full max-w-fit cursor-pointer overflow-hidden rounded border-2 border-solid border-black shadow-lg hover:bg-green-hover hover:ring"
                 >
                   <div className="py-2 px-2 text-center">
-                    <a className="xs:text-x font-bold lg:text-base">Review</a>
+                    <a className="xs:text-x font-bold lg:text-base">View Details</a>
                   </div>
                 </div>
-              )}
-              {isHomeChef && type === ORDER_TYPE.PENDING_ORDERS && (
-                <div
-                  onClick={() => {
-                    setStatus('O');
-                    setOrderId(order.id);
-                    setShowModal(true);
-                  }}
-                  className="hover:border-green w-full max-w-fit cursor-pointer overflow-hidden rounded border-2 border-solid border-green-light bg-green-light shadow-lg hover:bg-green-hover hover:ring"
-                >
-                  <div className="py-2 px-2 text-center">
-                    <a className="xs:text-x font-bold text-white lg:text-base">Accept Order</a>
+                {type === ORDER_TYPE.PAST_ORDERS && (
+                  <div
+                    onClick={() => handleReview(order.homechef_id)}
+                    className="w-full max-w-fit cursor-pointer overflow-hidden rounded border-2 border-solid border-black shadow-lg hover:bg-green-hover hover:ring"
+                  >
+                    <div className="py-2 px-2 text-center">
+                      <a className="xs:text-x font-bold lg:text-base">Review</a>
+                    </div>
                   </div>
-                </div>
-              )}
-              {isHomeChef && type === ORDER_TYPE.ONGOING_ORDERS && (
-                <div
-                  onClick={() => {
-                    setStatus('F');
-                    setOrderId(order.id);
-                    setShowModal(true);
-                  }}
-                  className="hover:border-green w-full max-w-fit cursor-pointer overflow-hidden rounded bg-green-light shadow-lg hover:bg-green-hover hover:ring"
-                >
-                  <div className="py-2 px-2 text-center">
-                    <a className="xs:text-x font-bold text-white lg:text-base">Fulfill Order</a>
+                )}
+                {isHomeChef && type === ORDER_TYPE.PENDING_ORDERS && (
+                  <div
+                    onClick={() => {
+                      setStatus('O');
+                      setOrderId(order.id);
+                      setShowModal(true);
+                    }}
+                    className="hover:border-green w-full max-w-fit cursor-pointer overflow-hidden rounded border-2 border-solid border-green-light bg-green-light shadow-lg hover:bg-green-hover hover:ring"
+                  >
+                    <div className="py-2 px-2 text-center">
+                      <a className="xs:text-x font-bold text-white lg:text-base">Accept Order</a>
+                    </div>
                   </div>
-                </div>
-              )}
-              {type === ORDER_TYPE.PENDING_ORDERS && (
-                <div
-                  onClick={() => {
-                    setStatus('C');
-                    setOrderId(order.id);
-                    setShowModal(true);
-                  }}
-                  className="w-full max-w-fit cursor-pointer overflow-hidden rounded bg-red-500 shadow-lg hover:bg-black hover:ring"
-                >
-                  <div className="py-2 px-2 text-center">
-                    <a className="xs:text-x font-bold text-white lg:text-base">
-                      {isHomeChef ? `Reject Order` : `Cancel Order`}
-                    </a>
+                )}
+                {isHomeChef && type === ORDER_TYPE.ONGOING_ORDERS && (
+                  <div
+                    onClick={() => {
+                      setStatus('F');
+                      setOrderId(order.id);
+                      setShowModal(true);
+                    }}
+                    className="hover:border-green w-full max-w-fit cursor-pointer overflow-hidden rounded bg-green-light shadow-lg hover:bg-green-hover hover:ring"
+                  >
+                    <div className="py-2 px-2 text-center">
+                      <a className="xs:text-x font-bold text-white lg:text-base">Fulfill Order</a>
+                    </div>
                   </div>
-                </div>
-              )}
+                )}
+                {type === ORDER_TYPE.PENDING_ORDERS && (
+                  <div
+                    onClick={() => {
+                      setStatus('C');
+                      setOrderId(order.id);
+                      setShowModal(true);
+                    }}
+                    className="w-full max-w-fit cursor-pointer overflow-hidden rounded bg-red-500 shadow-lg hover:bg-black hover:ring"
+                  >
+                    <div className="py-2 px-2 text-center">
+                      <a className="xs:text-x font-bold text-white lg:text-base">
+                        {isHomeChef ? `Reject Order` : `Cancel Order`}
+                      </a>
+                    </div>
+                  </div>
+                )}
+              </div>
+              <hr className="mt-4" />
             </div>
           );
         })
