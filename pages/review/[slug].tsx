d583c2d68c37451af3d0b298061874f6be39bf52
JH <>
2022-08-10 11:51:32 -0400

    enhancements to kitchen, review

--- pages/review/[slug].tsx
+++ pages/review/[slug].tsx
@@ -37,7 +37,7 @@ export const getServerSideProps = withPageAuth({
 
       let { data: review, error } = await supabase
         .from('Review')
-        .select(`id, consumer:consumer_id(id, name), time_posted, content, rating`)
+        .select(`id, consumer:consumer_id(id, name), time_posted, time_edited, content, rating`)
         .eq('homechef_id', homechef_id);
 
       if (error) throw error.message;
@@ -105,6 +105,7 @@ const Review: NextPage = ({ review, homeChef }: any) => {
         {
           content: data.content,
           rating: data.rating,
+          time_edited: new Date(),
         },
       ])
       .eq('id', reviewId);
@@ -162,10 +163,53 @@ const Review: NextPage = ({ review, homeChef }: any) => {
                 <div className="">
                   <span className="font-semibold">{review.consumer.name}</span>{' '}
                 </div>
-                <div className="flex">Rating: {review.rating}/5</div>
+                <div className="flex">
+                  {[...Array(5)].map((star, index) => {
+                    return index < review.rating ? (
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="h-6 w-6"
+                        fill="currentColor"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                        strokeWidth={2}
+                        key={index}
+                      >
+                        <title>{review.rating}/5</title>
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"
+                        />
+                      </svg>
+                    ) : (
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="h-6 w-6"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                        strokeWidth={2}
+                        key={index}
+                      >
+                        <title>{review.rating}/5</title>
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"
+                        />
+                      </svg>
+                    );
+                  })}
+                </div>
               </div>
               <div className="flex">
-                <div className="text-sm">{new Date(review.time_posted).toDateString()}</div>
+                <div className="text-sm">
+                  {new Date(review.time_posted).toDateString()}{' '}
+                  {review.time_edited && (
+                    <span>(Edited: {new Date(review.time_edited).toDateString()})</span>
+                  )}
+                </div>
               </div>
               <div className="flex place-content-between">
                 <div className="">{review.content}</div>
